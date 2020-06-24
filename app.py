4dae82e3aebcf3da505d3c09e3c9292d4c4fb0cc
Yuvraj <ysahu.atx@gmail.com>
2020-06-24 10:19:49 -0500

    Added login and signup pages with links

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
